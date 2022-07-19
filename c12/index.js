a74feb255387b7b0d51383565dd3218862f69576
Cvetanka Hadjiev <cvetanka888@yahoo.com>
2022-07-20 00:21:15 +0200

    query, projection, skip, sort - Studio 3T for MongoDB

---
+++ c12/index.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://username:password@cluster0.qj0lf6h.mongodb.net/baza1?retryWrites=true&w=majority';
+
+const connect = (connectionString) => {
+    return new Promise((success, fail) => {
+        mongoose.connect(connectionString, err => {
+            if (err) return fail(err);
+            console.log('Connected successfully to DB!');
+            return success();
+        });
+    });
+};
+
+const Studenti = mongoose.model(
+    'studenti', // model name
+    {
+        ime: String,
+        prezime: String,
+        prosek: Number
+    },
+    'studenti' // collection name
+);
+
+connect(connectionString)
+    .then(() => {
+    return Studenti.find({});
+})
+    .then(res => {
+        console.log(res);
+        let s = new Studenti({
+            ime: "Ivan",
+            prezime: "Ivanovski",
+            prosek: 7.3
+        });
+        return s.save()
+        .then(res => {
+            console.log('SAVED!', res);
+            return Studenti.updateOne({_id: '62d6ef3a3af668760fb896da'}, {prezime: 'Petrovski'})
+            .then(res => {
+                console.log('UPDATE FINISHED');
+                return Studenti.deleteOne({_id: '62d70ac81b3328e49605abc6'});
+            })
+            .then(res => { // ova e za koga pravime kolekcija na podatoci
+                console.log('DELETED RECORD', res);
+                return Studenti.find({ime: 'Ivan'}, {prezime: 1});
+            })
+            .then(res => {
+                console.log('FILTERED RESULTS:', res);
+                return Studenti.find({prosek: {'$gte': 9} }, { prezime: 1, prosek: 1});
+            })
+            .then(res => {
+                console.log('FILTERED RESULTS 2', res)
+            })
+    })
+    })
+    .catch(err => {
+        console.log(err);
+    });
