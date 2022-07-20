0166faf94975e7992d2a1c80e4c811a0f93f220f
Cvetanka Hadjiev <cvetanka888@yahoo.com>
2022-07-21 00:51:56 +0200

    worked on the homework assignment - query, projection, sort, limmit(MongoDB)

--- c12/index.js
+++ c12/index.js
@@ -1,63 +1,63 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://cvetankah:cvetanka1992@cluster0.qj0lf6h.mongodb.net/baza1?retryWrites=true&w=majority';
+// const connectionString = 'mongodb+srv://cvetankah:cvetanka1992@cluster0.qj0lf6h.mongodb.net/baza1?retryWrites=true&w=majority';
 
-const connect = (connectionString) => {
-    return new Promise((success, fail) => {
-        mongoose.connect(connectionString, err => {
-            if (err) return fail(err);
-            console.log('Connected successfully to DB!');
-            return success();
-        });
-    });
-};
+// const connect = (connectionString) => {
+//     return new Promise((success, fail) => {
+//         mongoose.connect(connectionString, err => {
+//             if (err) return fail(err);
+//             console.log('Connected successfully to DB!');
+//             return success();
+//         });
+//     });
+// };
 
-const Studenti = mongoose.model(
-    'studenti', // model name
-    {
-        ime: String,
-        prezime: String,
-        prosek: Number
-    },
-    'studenti' // collection name
-);
+// const Studenti = mongoose.model(
+//     'studenti', // model name
+//     {
+//         ime: String,
+//         prezime: String,
+//         prosek: Number
+//     },
+//     'studenti' // collection name
+// );
 
-connect(connectionString)
-    .then(() => {
-    return Studenti.find({});
-})
-    .then(res => {
-        console.log(res);
-        let s = new Studenti({
-            ime: "Ivan",
-            prezime: "Ivanovski",
-            prosek: 7.3
-        });
-        return s.save()
-        .then(res => {
-            console.log('SAVED!', res);
-            return Studenti.updateOne({_id: '62d7317e3af668760fb89700'}, {prezime: 'Petrovski'})
-            .then(res => {
-                console.log('UPDATE FINISHED');
-                return Studenti.deleteOne({_id: '62d733c43af668760fb89702'});
-            })
-            .then(res => { // ova e za koga pravime kolekcija na podatoci
-                console.log('DELETED RECORD', res);
-                return Studenti.find({ime: 'Ivan'}, {prezime: 1});
-            })
-            .then(res => {
-                console.log('FILTERED RESULTS:', res);
-                return Studenti.find({prosek: {'$gte': 9} }, { prezime: 1, prosek: 1});
-            })
-            .then(res => {
-                console.log('FILTERED RESULTS 2', res);
-                return Studenti.deleteMany({ime: 'Ivann'});
-            })
-            .then(res => {
-                console.log('FILTERED RESULTS 3', res);
-            })
-    })
-    })
-    .catch(err => {
-        console.log(err);
-    });
+// connect(connectionString)
+//     .then(() => {
+//     return Studenti.find({}); // vo find({}) treba da go definiram toa   sto sakam da mi bide vrateno. Toa nesto e Promise, sto znaci deka treba da se stavi .then/res/catch i eden console.log() za da mozam da gi citam podatocite
+// })
+//     .then(res => {
+//         console.log(res);
+//         let s = new Studenti({
+//             ime: "Ivan",
+//             prezime: "Ivanovski",
+//             prosek: 7.3
+//         });
+//         return s.save()
+//         .then(res => {
+//             console.log('SAVED!', res);
+//             return Studenti.updateOne({_id: '62d7317e3af668760fb89700'}, {prezime: 'Petrovski'})
+//             .then(res => {
+//                 console.log('UPDATE FINISHED');
+//                 return Studenti.deleteOne({_id: '62d733c43af668760fb89702'});
+//             })
+//             .then(res => { // ova e za koga pravime kolekcija na podatoci
+//                 console.log('DELETED RECORD', res);
+//                 return Studenti.find({ime: 'Ivan'}, {prezime: 1});
+//             })
+//             .then(res => {
+//                 console.log('FILTERED RESULTS:', res);
+//                 return Studenti.find({prosek: {'$gte': 9} }, { prezime: 1, prosek: 1});
+//             })
+//             .then(res => {
+//                 console.log('FILTERED RESULTS 2', res);
+//                 return Studenti.deleteMany({ime: 'Ivann'});
+//             })
+//             .then(res => {
+//                 console.log('FILTERED RESULTS 3', res);
+//             })
+//     })
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
---
+++ c12/index_homework.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://...:...@.../baza1?retryWrites=true&w=majority';
+
+const connect = (connectionString) => {
+    return new Promise((success, fail) => {
+        mongoose.connect(connectionString, err => {
+            if (err) return fail(err);
+            console.log('Connected successfully to DB!');
+            return success();
+        });
+    });
+};
+
+const Studenti = mongoose.model(
+    'studenti', // model name
+    {
+        ime: String,
+        prezime: String,
+        prosek: Number,
+        lokacija: {
+            grad: String,
+            drzava: String
+        }
+    },
+    'studenti' // collection name
+);
+
+connect(connectionString)
+    .then(() => {
+    return Studenti.find({prosek: {'$gt': 9.0,}}, {ime: 1, prezime: 1, prosek: 1,}).sort({prosek: -1}).limit(5);
+})
+.then( res => {
+    console.log('Топ 5 најдобри студенти по просек, почнувајќи од најдобриот:', res);
+    return Studenti.find({prosek: {'$lte': 8.5}, 'lokacija.grad': "Skopje"}, {ime: 1, prezime: 1, prosek: 1, 'lokacija.grad': "Skopje"}).sort({prosek: 1}).limit(3); 
+})
+.then(res => {
+    console.log('Топ 3 најлоши студенти од Скопје', res);
+    return Studenti.find({prosek: {'$lte': 10.0}, 'lokacija.grad': "Skopje"}, {ime:1, prezime:1, prosek: 1, 'lokacija.grad': "Skopje"}).sort({prosek: -1}).limit(10);
+})
+.then(res => {
+    console.log('Најдобри 10 студенти во Скопје:',res);
+    return Studenti.find({prosek: {'$lte': 10.0}, 'lokacija.drzava': "Makedonija"}, {ime:1, prezime:1, 'lokacija.grad': 1}).sort({prosek: -1}).limit(3);
+})
+.then(res => {
+    console.log('Најдобри 3 студенти во Македонија', res);
+    return Studenti.find({'$lte': 10.0, 'lokacija.grad': "Bitola"}, {prezime: 1, prosek: 1}).sort({prosek: -1}).limit(5);
+})
+.then(res => {
+    console.log('Најдобри 5 студенти од Битола', res);
+    return Studenti.find({'lokacija.grad': "Bitola"}, {prezime: 1, 'lokacija.grad': "Bitola"}).sort({prezime: 1});
+})
+.then(res => {
+    console.log('Приказ на студенти од Битола подредени по презиме', res);
+    return Studenti.find({'lokacija.grad': "Kumanovo"}, {ime: 1, 'lokacija.grad': "Kumanovo"}).sort({ime: 1});
+})
+.then(res => {
+    console.log('Приказ на студенти од Куманово подредени по име', res);
+    return Studenti.find({'$lte': 10.0, 'lokacija.drzava': "Makedonija"}, {ime: 1, prezime: 1, prosek: 1, lokacija: 1}).sort({prosek: -1}).limit(1);
+})
+.then(res => {
+    console.log('Најдобар студент во Македонија', res);
+})
+    .catch(err => {
+         console.log(err);
+});
